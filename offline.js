eae5a01d1e82239d153689f5fd2983878dd983f0
mookpailin29289 <52027014+mookpailin29289@users.noreply.github.com>
2019-09-12 14:59:59 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1568274206,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/dino-sheet0.png",
+		"images/dino-sheet1.png",
+		"images/dino-sheet2.png",
+		"images/dino-sheet3.png",
+		"images/sprite-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite5-sheet1.png",
+		"images/sprite5-sheet2.png",
+		"images/sprite5-sheet3.png",
+		"images/sprite6-sheet0.png",
+		"images/hpdino-sheet0.png",
+		"images/sprite8-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
